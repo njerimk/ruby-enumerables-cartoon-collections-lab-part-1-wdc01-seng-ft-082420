b10ef22a6d9973db19415e362500034504ab5bde
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-15 17:24:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def list_dwarves(array)
   dwarves = ["Dopey", "Grumpy", "Bashful"]
   
   i= 1
-  dwarves.each_with_index |dwarves|
+  dwarves.each_with_index |dwarves| do
     puts i + dwarves
     i += 1.to_s + ".*" + dwarves
     i =+ 1
