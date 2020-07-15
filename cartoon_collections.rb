18d3e15581ed252cb0db3adbee260a6df497932b
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-15 17:14:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,8 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   dwarves = ["Dopey", "Grumpy", "Bashful"]
-    dwarves.each
+    dwarves.each do |dwarves|
+      puts "Hello there Mr." + dwarves
 end
 
 def list_dwarves(array)
